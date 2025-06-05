a00286dc2f9826b10ffa189da9d96f83de30d65e
slavikm977 <33388450+slavikm777@users.noreply.github.com>
2025-06-05 22:42:07 +0300

    Добавьте файлы проекта.

---
+++ Pol_company/Pol_company.cpp
@@ -0,0 +1,9 @@
+﻿// Pol_company.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!\n";
+}
+
